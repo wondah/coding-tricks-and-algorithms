29c9a5528b821a6883def0d49f6c5c3df26d46eb
nem <n3m4nj4k0k4@gmail.com>
2014-09-24 23:11:29 -0400

    UniqueChars.java

--- UniqueChars.java
+++ UniqueChars.java
@@ -14,9 +14,28 @@ public class UniqueChars {
 	/*
 	 *  N - length of the string
 	 * Time Complexity:		O(N)
-	 * Space Complexity:	O(1)
+	 * Space Complexity:	O(N)	(HashMap storage)
 	 */
 	public static boolean hasUniqueChars1(String s) {
+		if(s.length() > 128) {
+			return false;
+		}
+		HashMap<Character, Boolean> map = new HashMap<Character, Boolean>();
+		for(int i = 0; i < s.length(); i++) {
+			if(map.get(s.charAt(i)) != null) {
+				return false;
+			}
+			map.put(s.charAt(i), true);
+		}
+		return true;
+	}
+	
+	/*
+	 *  N - length of the string
+	 * Time Complexity:		O(N)
+	 * Space Complexity:	O(1)
+	 */
+	public static boolean hasUniqueChars2(String s) {
 		if(s.length() > 128) { // 128 - size of ASCII chart
 			return false;
 		}
@@ -29,14 +48,13 @@ public class UniqueChars {
 			uniqueChars[curr] = true;
 		}
 		return true;
-	}
 	
 	/*
 	 *  N - length of the string
 	 * Time Complexity:		O(N)
 	 * Space Complexity:	O(1)	(reduced by a factor of 8)
 	 */
-	public static boolean hasUniqueChars2(String s) {
+	public static boolean hasUniqueChars3(String s) {
 		if(s.length() > 128) { // 128 - size of ASCII chart
 			return false;
 		}
@@ -47,25 +65,6 @@ public class UniqueChars {
 				return false;
 			}
 			bitvector |= (1 << asciiVal);
-		}
-		return true;
-	}
-	
-	/*
-	 *  N - length of the string
-	 * Time Complexity:		O(N)
-	 * Space Complexity:	O(N)	(HashMap storage)
-	 */
-	public static boolean hasUniqueChars3(String s) {
-		if(s.length() > 128) {
-			return false;
-		}
-		HashMap<Character, Boolean> map = new HashMap<Character, Boolean>();
-		for(int i = 0; i < s.length(); i++) {
-			if(map.get(s.charAt(i)) != null) {
-				return false;
-			}
-			map.put(s.charAt(i), true);
 		}
 		return true;
 	}
